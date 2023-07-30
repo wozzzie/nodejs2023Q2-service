675529149dc9fa2cf85b9d137cc3dc52b4abf82e
Maria Gornashevich <lightbrown9874@gmail.com>
2023-07-31 02:19:33 +0300

    feat: add dto and custom exeptions

---
+++ src/utils/exceptions.ts
@@ -0,0 +1,35 @@
+import {
+  BadRequestException,
+  ForbiddenException,
+  NotFoundException,
+} from '@nestjs/common';
+
+export class LoginAndPasswordRequiredException extends BadRequestException {
+  constructor() {
+    super('Login and password are required');
+  }
+}
+
+export class InvalidOldPasswordException extends ForbiddenException {
+  constructor() {
+    super('Invalid old password');
+  }
+}
+
+export class InvalidDataException extends BadRequestException {
+  constructor() {
+    super('Invalid data');
+  }
+}
+
+export class UserNotFoundException extends NotFoundException {
+  constructor() {
+    super('User not found');
+  }
+}
+
+export class InvalidUserIdException extends BadRequestException {
+  constructor() {
+    super('Invalid userId');
+  }
+}
