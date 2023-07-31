efe996ad35ec982ba027f704f14c5fd72394b77b
Maria Gornashevich <lightbrown9874@gmail.com>
2023-07-31 17:57:20 +0300

    feat: add dto for artists

--- src/tracks/dto/updateTrackDto.dto.ts
+++ src/tracks/dto/updateTrackDto.dto.ts
@@ -1,6 +1,6 @@
 export class UpdateTrackDto {
-  name: string;
-  artistId: string | null;
-  albumId: string | null;
-  duration: number;
+  name?: string;
+  artistId?: string | null;
+  albumId?: string | null;
+  duration?: number;
 }
